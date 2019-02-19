93d1ac362931d38b684628083086c3623e0a6f2d
Michel Manarin <MichelManarin@gmail.com>
2019-02-18 21:40:14 -0300

    Commit inicial
    Commit inicial

---
+++ precache-manifest.74523d0ee298281776bf2bd08994ef88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "ef17c1e228a42710fdac",
+    "url": "/static/css/main.6536cf26.chunk.css"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "d1f70ae2c1bdf4bf9f2f",
+    "url": "/static/js/1.d1f70ae2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef17c1e228a42710fdac",
+    "url": "/static/js/main.ef17c1e2.chunk.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d1f70ae2c1bdf4bf9f2f",
+    "url": "/static/css/1.58ac6baa.chunk.css"
+  },
+  {
+    "revision": "6f611de0220f42616a5aa3010e318dbd",
+    "url": "/index.html"
+  }
+];
